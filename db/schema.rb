02eee769362f34326fb5d669d612a139c0182465
Yagi Hitsuji <84sena0921@gmail.com>
2019-07-10 15:31:12 +0900

    Add: Favoriteモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_04_073228) do
+ActiveRecord::Schema.define(version: 2019_07_08_063941) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_favorites_on_post_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "games", force: :cascade do |t|
     t.string "title"
