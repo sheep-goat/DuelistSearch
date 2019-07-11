a1e9a24cad17bc274c5f3865a399fd38a3db3364
Yagi Hitsuji <84sena0921@gmail.com>
2019-07-11 15:23:18 +0900

    Add: Postのバリデーションの追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,10 @@ class Post < ApplicationRecord
   belongs_to :game
   belongs_to :prefecture
   has_many :favorites, dependent: :destroy
+
+  validates :user_id, presence: true
+  validates :game_id, presence: true
+  validates :title, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :schedule_date, presence: true
+  validates :max_participant, numericality: { only_integer: true, greater_than: 0 }
 end
