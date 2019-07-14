f1022f7779464a96fedf33a179b9642f4a30e25c
Yagi Hitsuji <84sena0921@gmail.com>
2019-07-14 18:38:35 +0900

    Test: ユーザIDに「-」が含まれる場合のテストを作成

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -13,4 +13,9 @@ RSpec.describe User, type: :model do
     @user.username = ''
     expect(@user.valid?).to be_falsey
   end
+
+  it 'is invalid when the username contains "-"' do
+    @user.username = 'test-test'
+    expect(@user.valid?).to be_falsey
+  end
 end
