0e8a0041b697ad63bc464ea12188e9503b8acc7f
Yagi Hitsuji <84sena0921@gmail.com>
2019-07-16 00:03:33 +0900

    Test: ユーザIDの文字数に関するテストを作成

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -27,4 +27,24 @@ RSpec.describe User, type: :model do
     @user.username = dup_username
     expect(@user.valid?).to be_falsey
   end
+
+  it 'is invalid when username is less than 2 characters' do
+    @user.username = 'te'
+    expect(@user.valid?).to be_falsey
+  end
+
+  it 'is valid when username is more than 3 characters' do
+    @user.username = 'tes'
+    expect(@user.valid?).to be_truthy
+  end
+
+  it 'is valid when username is less than 20 characters' do
+    @user.username = 'test_test_test_test1'
+    expect(@user.valid?).to be_truthy
+  end
+
+  it 'is invalid when username is more than 21 characters' do
+    @user.username = 'test_test_test_test_t'
+    expect(@user.valid?).to be_falsey
+  end
 end
