1482be33e294d4e8c49aa7b2ac13a8875c58914b
Yagi Hitsuji <84sena0921@gmail.com>
2019-07-20 16:34:57 +0900

    :bug: Fix: データベースの修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_20_011128) do
+ActiveRecord::Schema.define(version: 2019_07_08_063941) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -57,7 +57,6 @@ ActiveRecord::Schema.define(version: 2019_07_20_011128) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "image_path"
   end
 
 end
