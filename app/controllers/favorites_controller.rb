8aae77c9bb1eb3ae6cfa7d10cdee324b03e591da
Yagi Hitsuji <84sena0921@gmail.com>
2019-07-25 10:38:17 +0900

    :+1: Fix: お気に入り登録機能の非同期化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,12 +1,23 @@
 class FavoritesController < ApplicationController
+  before_action :set_variables
+
   def create
     post = Post.find(params[:id])
     fav = current_user.favorites.new(post_id: post.id)
     fav.save
+    @fav_users = Favorite.where(post_id: params[:id]).count
   end
 
   def destroy
     fav = current_user.favorites.find_by(post_id: params[:id])
     fav.destroy
+    @fav_users = Favorite.where(post_id: params[:id]).count
+  end
+
+  private
+
+  def set_variables
+    @post = Post.find(params[:id])
+    @fav_link_id = "#fav-link-#{@post.id}"
   end
 end
