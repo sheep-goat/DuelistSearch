5bb419a17b186a9602be1421c8e7119d5af17b5b
Yagi Hitsuji <84sena0921@gmail.com>
2019-10-19 16:53:23 +0900

    :art: Add: 投稿のアイコンに募集中/募集終了のリボンを表示(#募集中/募集終了が分かりやすいデザインに変更)

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,4 +2,15 @@ module PostsHelper
   def format_date(date)
     date.strftime(t('time.formats.post'))
   end
+
+  def deadline(schedule)
+    return "expired" if expired?(schedule)
+    return "recruting"
+  end
+
+  def expired?(schedule)
+    now = Time.now
+    return true if now >= schedule
+    return false
+  end
 end
