3d3da40b6c19d59c5eded4f218e482926c800d7f
Yagi Hitsuji <84sena0921@gmail.com>
2019-10-23 15:12:24 +0900

    :+1: Fix: 評価管理用テーブルの仕様変更(#参加者専用ページの作成)

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_20_142107) do
+ActiveRecord::Schema.define(version: 2019_10_23_060941) do
 
   create_table "assessments", force: :cascade do |t|
-    t.integer "participator_id", null: false
     t.string "grade", null: false
     t.string "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["participator_id"], name: "index_assessments_on_participator_id"
+    t.integer "user_id"
+    t.integer "post_id"
+    t.index ["post_id"], name: "index_assessments_on_post_id"
+    t.index ["user_id"], name: "index_assessments_on_user_id"
   end
 
   create_table "comments", force: :cascade do |t|
